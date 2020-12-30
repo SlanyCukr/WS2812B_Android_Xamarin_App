31d28a38fce12d4285860607bdaed71760b7032e
Marek Hovadík <marek.hovadik@seznam.cz>
2020-12-30 12:59:44 +0100

    Working example

--- WS2812B_Android_Xamarin_App/MainActivity.cs
+++ WS2812B_Android_Xamarin_App/MainActivity.cs
@@ -3,6 +3,8 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Net.Http;
+using Xamarin.Essentials;
 
 namespace WS2812B_Android_Xamarin_App
 {
@@ -15,6 +17,27 @@ namespace WS2812B_Android_Xamarin_App
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button turnOnButton = FindViewById<Button>(Resource.Id.TurnOnButton);
+            Button turnOffButton = FindViewById<Button>(Resource.Id.TurnOffButton);
+            Button rainbowButton = FindViewById<Button>(Resource.Id.RainbowButton);
+
+            turnOnButton.Click += async (sender, e) =>
+            {
+                var client = new HttpClient();
+                await client.GetAsync("http://192.168.0.114/turn_on");
+            };
+            turnOffButton.Click += async (sender, e) =>
+            {
+                var client = new HttpClient();
+                await client.GetAsync("http://192.168.0.114/turn_off");
+
+            };
+            rainbowButton.Click += (sender, e) =>
+            {
+                var client = new HttpClient();
+                client.GetAsync("http://192.168.0.114/rainbow");
+            };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
